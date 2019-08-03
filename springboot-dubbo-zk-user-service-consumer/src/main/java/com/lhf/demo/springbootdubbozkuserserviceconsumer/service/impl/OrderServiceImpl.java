73de5555b5ec9f630f1bec904eabc1f977602d49
刘宏飞 <liuhongfei@quanmeishi.cn>
2019-08-03 17:48:50 +0800

    Fix

--- springboot-dubbo-zk-user-service-consumer/src/main/java/com/lhf/demo/springbootdubbozkuserserviceconsumer/service/impl/OrderServiceImpl.java
+++ springboot-dubbo-zk-user-service-consumer/src/main/java/com/lhf/demo/springbootdubbozkuserserviceconsumer/service/impl/OrderServiceImpl.java
@@ -1,7 +1,7 @@
 package com.lhf.demo.springbootdubbozkuserserviceconsumer.service.impl;
 
-import com.lhf.demo.springbootdubbozkcommon.service.order.OrderService;
 import com.lhf.demo.springbootdubbozkcommon.entity.user.UserAddress;
+import com.lhf.demo.springbootdubbozkcommon.service.order.OrderService;
 import com.lhf.demo.springbootdubbozkcommon.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
